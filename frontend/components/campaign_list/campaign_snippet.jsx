faf3b602a9db568ea1dac3eb9e6e17aea055ab59
Arthur Yip <arthuryip723@gmail.com>
2016-11-20 15:15:34 -0500

    applied react-bootstrap

--- frontend/components/campaign_list/campaign_snippet.jsx
+++ frontend/components/campaign_list/campaign_snippet.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 const CampaignSnippet = ({ campaign }) => (
-  <div>Name: {campaign.name}</div>
+  <div>Name: {campaign.name}<Button>Remove</Button></div>
 );
 
 export default CampaignSnippet;
