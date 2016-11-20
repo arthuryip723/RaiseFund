16c6de8b72c0b43d9b27f9d8a990293084919d9a
Arthur Yip <arthuryip723@gmail.com>
2016-11-20 17:06:54 -0500

    used links

--- frontend/components/campaign_list/campaign_snippet.jsx
+++ frontend/components/campaign_list/campaign_snippet.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router';
 import { Button } from 'react-bootstrap';
 
 const CampaignSnippet = ({ campaign }) => (
-  <div>Name: {campaign.name}<Button>Remove</Button></div>
+  <div><Link to={`/campaigns/${campaign.id}`}>Name: {campaign.name}</Link><Button>Remove</Button></div>
 );
 
 export default CampaignSnippet;
